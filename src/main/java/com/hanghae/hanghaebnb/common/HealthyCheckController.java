2cd2ed59d98d19999e5906a645c7a410da32325a
yjk <djwhs415@gmail.com>
2022-12-24 19:34:30 +0900

    feat : CI/CD TEST

---
+++ src/main/java/com/hanghae/hanghaebnb/common/HealthyCheckController.java
@@ -0,0 +1,18 @@
+package com.hanghae.hanghaebnb.common;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HealthyCheckController {
+
+    @GetMapping
+    public ResponseEntity<HttpStatus> healthyCheck() {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
