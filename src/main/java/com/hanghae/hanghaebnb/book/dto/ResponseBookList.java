6de8bacc7f1c2f09fbb0f5f93bd561d0290347d8
김예진 <a89557400@gmail.com>
2022-12-26 14:06:17 +0900

    feat : 예약 조회 API 추가

---
+++ src/main/java/com/hanghae/hanghaebnb/book/dto/ResponseBookList.java
@@ -0,0 +1,18 @@
+package com.hanghae.hanghaebnb.book.dto;
+
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+public class ResponseBookList {
+
+    private final List<ResponseBook> responseBookList = new ArrayList<>();
+
+    public void addBook(ResponseBook responseBook){
+        responseBookList.add(responseBook);
+    }
+
+}
+
--- src/main/java/com/hanghae/hanghaebnb/book/mapper/BookMapper.java
+++ src/main/java/com/hanghae/hanghaebnb/book/mapper/BookMapper.java
@@ -3,19 +3,20 @@ package com.hanghae.hanghaebnb.book.mapper;
 import com.hanghae.hanghaebnb.book.dto.RequestBook;
 import com.hanghae.hanghaebnb.book.entity.Book;
 import com.hanghae.hanghaebnb.room.entity.Room;
+import com.hanghae.hanghaebnb.users.entity.Users;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BookMapper {
 
-    public Book toBook(Room room, RequestBook requestBook, Long total){
+    public Book toBook(Room room, RequestBook requestBook, Long total, Users users){
         return Book.builder()
                 .headCount(requestBook.getHeadCount())
                 .totalPrice(total)       //추가해야함
                 .checkIn(requestBook.getCheckIn())
                 .checkOut(requestBook.getCheckOut())
                 .room(room)
-//                .users()        //추가 예정
+                .users(users)        //추가 예정
                 .build();
     }
 
