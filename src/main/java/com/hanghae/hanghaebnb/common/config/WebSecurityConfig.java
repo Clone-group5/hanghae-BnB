62e85f9abecc4194a9dfaebb20408fe1f833e018
hjun0917 <hjun0917@naver.com>
2022-12-26 21:57:14 +0900

    [feat] CORS 기본 설정

---
+++ src/main/java/com/hanghae/hanghaebnb/common/config/WebSecurityConfig.java
@@ -0,0 +1,51 @@
+package com.hanghae.hanghaebnb.common.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity // 스프링 Security 지원을 가능하게 함
+public class WebSecurityConfig {
+
+    // BCrypt 형식의 암호화 방식, 적응형 단방향 암호화
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        // CSRF 설정
+        http.csrf().disable();
+
+        // "/api" 경로를 타고 들어온 모든 요청에 대해 허가.
+        http.authorizeRequests().antMatchers("/api/**").permitAll()
+                .anyRequest().authenticated();
+
+        return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("http://localhost:3000");
+        configuration.addAllowedMethod("*"); // 허용할 Http Method
+        configuration.addAllowedHeader("*");
+        configuration.setAllowCredentials(true); // 내 서버가 응답할 때 json을 js에서 처리할 수 있게 설정
+        configuration.setMaxAge(3600L);
+        configuration.addExposedHeader("Authorization"); // 헤더에 있는 JWT 토큰을 클라이언트에서 사용할 수 있도록 권한을 주는 부분
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/api/**", configuration);
+        return source;
+    }
+}
