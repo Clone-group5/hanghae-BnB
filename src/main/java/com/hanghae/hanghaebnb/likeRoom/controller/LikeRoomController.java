2242366f492c39601fa5ec58c2936fd155428cac
김예진 <a89557400@gmail.com>
2022-12-27 22:19:52 +0900

    refactor : 좋아요 api 유저확인 추가

--- src/main/java/com/hanghae/hanghaebnb/likeRoom/controller/LikeRoomController.java
+++ src/main/java/com/hanghae/hanghaebnb/likeRoom/controller/LikeRoomController.java
@@ -2,10 +2,12 @@ package com.hanghae.hanghaebnb.likeRoom.controller;
 
 import com.hanghae.hanghaebnb.book.service.BookService;
 import com.hanghae.hanghaebnb.common.dto.ResponseDto;
+import com.hanghae.hanghaebnb.common.security.UserDetailsImpl;
 import com.hanghae.hanghaebnb.likeRoom.service.LikeRoomService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,8 +21,8 @@ public class LikeRoomController {
     private final LikeRoomService likeRoomService;
 
     @PostMapping("/room/{roomId}/like")
-    public ResponseEntity<ResponseDto> likeRoom(@PathVariable Long roomId){
-        boolean like = likeRoomService.likeRoom(roomId);
+    public ResponseEntity<ResponseDto> likeRoom(@PathVariable Long roomId, @AuthenticationPrincipal UserDetailsImpl userDetails){
+        boolean like = likeRoomService.likeRoom(roomId, userDetails.getUsers());
         if(like){
             ResponseDto responseDto = new ResponseDto<>(200, "숙소 좋아요가 완료되었습니다.", like);
             return new ResponseEntity<>(responseDto, HttpStatus.OK);
