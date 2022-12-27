43f50a97df7e5f4e6c9f1d5d84a1632ce172fee4
syckor <syckor@naver.com>
2022-12-27 21:25:01 +0900

    Fix : repository 에러잡기

--- src/main/java/com/hanghae/hanghaebnb/room/repository/RoomRepository.java
+++ src/main/java/com/hanghae/hanghaebnb/room/repository/RoomRepository.java
@@ -3,6 +3,10 @@ package com.hanghae.hanghaebnb.room.repository;
 import com.hanghae.hanghaebnb.room.entity.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface RoomRepository extends JpaRepository<Room, Long> {
 
+    List<Room> findAllByLocation(String Location);
 }
