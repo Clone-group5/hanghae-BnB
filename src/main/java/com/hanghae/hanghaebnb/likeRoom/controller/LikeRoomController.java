ac22045fef7e31aee89fbc5eddfe1d21782f10c3
yejin0718 <83577996+yejin0718@users.noreply.github.com>
2022-12-27 23:31:04 +0900

    refactor : 좋아요 response 수정 (#71)

--- src/main/java/com/hanghae/hanghaebnb/likeRoom/controller/LikeRoomController.java
+++ src/main/java/com/hanghae/hanghaebnb/likeRoom/controller/LikeRoomController.java
@@ -3,6 +3,7 @@ package com.hanghae.hanghaebnb.likeRoom.controller;
 import com.hanghae.hanghaebnb.book.service.BookService;
 import com.hanghae.hanghaebnb.common.dto.ResponseDto;
 import com.hanghae.hanghaebnb.common.security.UserDetailsImpl;
+import com.hanghae.hanghaebnb.likeRoom.dto.ResponseLikeRoom;
 import com.hanghae.hanghaebnb.likeRoom.service.LikeRoomService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -22,13 +23,13 @@ public class LikeRoomController {
 
     @PostMapping("/room/{roomId}/like")
     public ResponseEntity<ResponseDto> likeRoom(@PathVariable Long roomId, @AuthenticationPrincipal UserDetailsImpl userDetails){
-        boolean like = likeRoomService.likeRoom(roomId, userDetails.getUsers());
-        if(like){
-            ResponseDto responseDto = new ResponseDto<>(200, "숙소 좋아요가 완료되었습니다.", like);
+        ResponseLikeRoom responseLikeRoom = likeRoomService.likeRoom(roomId, userDetails.getUsers());
+        if(responseLikeRoom.getLike()){
+            ResponseDto responseDto = new ResponseDto<>(200, "숙소 좋아요가 완료되었습니다.", responseLikeRoom);
             return new ResponseEntity<>(responseDto, HttpStatus.OK);
         }
         else{
-            ResponseDto responseDto = new ResponseDto<>(200, "숙소 좋아요가 취소되었습니다.", like);
+            ResponseDto responseDto = new ResponseDto<>(200, "숙소 좋아요가 취소되었습니다.", responseLikeRoom);
             return new ResponseEntity<>(responseDto, HttpStatus.OK);
         }
     }
