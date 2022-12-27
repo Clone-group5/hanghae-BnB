a11ebe63825bdcdede6a5241f0d004437664b189
yejin0718 <83577996+yejin0718@users.noreply.github.com>
2022-12-27 23:00:11 +0900

    refactor : 예약 api 로그인 확인 추가 (#69)

--- src/main/java/com/hanghae/hanghaebnb/book/controller/BookController.java
+++ src/main/java/com/hanghae/hanghaebnb/book/controller/BookController.java
@@ -4,9 +4,11 @@ import com.hanghae.hanghaebnb.book.dto.RequestBook;
 import com.hanghae.hanghaebnb.book.dto.ResponseBookList;
 import com.hanghae.hanghaebnb.book.service.BookService;
 import com.hanghae.hanghaebnb.common.dto.ResponseDto;
+import com.hanghae.hanghaebnb.common.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -19,8 +21,8 @@ public class BookController {
      * 예약 조회
      */
     @GetMapping("/book")
-    public ResponseEntity<ResponseDto> showBook(){
-        ResponseBookList responseBookList = bookService.showBook();
+    public ResponseEntity<ResponseDto> showBook(@AuthenticationPrincipal UserDetailsImpl userDetails){
+        ResponseBookList responseBookList = bookService.showBook(userDetails.getUsers());
         ResponseDto responseDto = new ResponseDto(200, "예약 정보 조회가 완료되었습니다.", responseBookList);
         return new ResponseEntity<>(responseDto, HttpStatus.OK);
     }
@@ -29,8 +31,8 @@ public class BookController {
      * 예약 등록
      */
     @PostMapping("/room/{roomId}/book")
-    public ResponseEntity<ResponseDto> addBook(@PathVariable Long roomId, @RequestBody RequestBook requestBook){
-        bookService.addBook(roomId, requestBook);
+    public ResponseEntity<ResponseDto> addBook(@PathVariable Long roomId, @RequestBody RequestBook requestBook, @AuthenticationPrincipal UserDetailsImpl userDetails){
+        bookService.addBook(roomId, requestBook, userDetails.getUsers());
         ResponseDto responseDto = new ResponseDto(200, "예약이 완료되었습니다.", null);
         return new ResponseEntity<>(responseDto, HttpStatus.OK);
     }
@@ -39,8 +41,8 @@ public class BookController {
      * 예약 취소
      */
     @DeleteMapping("/book/{bookId}")
-    public ResponseEntity<ResponseDto> deleteBook(@PathVariable Long bookId){
-        bookService.deleteBook(bookId);
+    public ResponseEntity<ResponseDto> deleteBook(@PathVariable Long bookId, @AuthenticationPrincipal UserDetailsImpl userDetails){
+        bookService.deleteBook(bookId, userDetails.getUsers());
         ResponseDto responseDto = new ResponseDto(200, "숙소 예약 취소가 완료되었습니다.", null);
         return new ResponseEntity<>(responseDto, HttpStatus.OK);
     }
