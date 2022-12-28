4ed19a3324ff8a2fb47c7e477df8ae46c261cd6b
syckor <syckor@naver.com>
2022-12-28 18:56:32 +0900

    Fix : delete기능에서 cascade되도록 수정

--- src/main/java/com/hanghae/hanghaebnb/likeRoom/repository/LikeRoomRepository.java
+++ src/main/java/com/hanghae/hanghaebnb/likeRoom/repository/LikeRoomRepository.java
@@ -4,11 +4,18 @@ import com.hanghae.hanghaebnb.likeRoom.entity.LikeRoom;
 import com.hanghae.hanghaebnb.room.entity.Room;
 import com.hanghae.hanghaebnb.users.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface LikeRoomRepository extends JpaRepository<LikeRoom,Long> {
 
     Optional<LikeRoom> findLikeRoomByRoomsAndUsers(Room room, Users users);
+
+    @Modifying
+    @Query("DELETE FROM LikeRoom c WHERE c.rooms = :room")
+    void deleteByRooms(@Param("room") Room room);
 
 }
