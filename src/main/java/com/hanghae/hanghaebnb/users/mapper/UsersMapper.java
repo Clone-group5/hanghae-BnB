b8170afcb44ff7f2d0e8856af7bee44074ab3670
Kim Hyeong jun <91590391+hjun0917@users.noreply.github.com>
2022-12-28 22:05:49 +0900

    feat : kakao OAuth (#91)

--- src/main/java/com/hanghae/hanghaebnb/users/mapper/UsersMapper.java
+++ src/main/java/com/hanghae/hanghaebnb/users/mapper/UsersMapper.java
@@ -14,4 +14,14 @@ public class UsersMapper {
                 .userRole(userRole)
                 .build();
     }
+
+    public Users toKakaoUsers(String email, String nickname, String password, UsersRoleEnum userRole, Long kakaoId) {
+        return Users.builder()
+                .email(email)
+                .nickname(nickname)
+                .password(password)
+                .userRole(userRole)
+                .kakaoId(kakaoId)
+                .build();
+    }
 }
--- src/main/java/com/hanghae/hanghaebnb/users/repository/UserRepository.java
+++ src/main/java/com/hanghae/hanghaebnb/users/repository/UserRepository.java
@@ -1,12 +1,11 @@
 package com.hanghae.hanghaebnb.users.repository;
 
-import com.hanghae.hanghaebnb.likeRoom.entity.LikeRoom;
-import com.hanghae.hanghaebnb.room.entity.Room;
 import com.hanghae.hanghaebnb.users.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<Users, Long> {
-        Optional<Users> findByEmail(String email);
+    Optional<Users> findByEmail(String email);
+    Optional<Users> findByKakaoId(Long kakaoId);
 }
