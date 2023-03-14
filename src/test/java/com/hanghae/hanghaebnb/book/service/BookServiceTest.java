a3ff01c53ee09e692524c105a20eb12bc785139b
yejin0718 <a89557400@email.com>
2023-03-14 12:19:33 +0900

    feat: 루토스 사용하한 동시성 제어 기능 추가

---
+++ src/test/java/com/hanghae/hanghaebnb/book/service/BookServiceTest.java
@@ -0,0 +1,78 @@
+package com.hanghae.hanghaebnb.book.service;
+
+import java.time.LocalDate;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.hanghae.hanghaebnb.book.dto.RequestBook;
+import com.hanghae.hanghaebnb.book.facade.LettuceRockBookFacade;
+import com.hanghae.hanghaebnb.book.repository.BookRepository;
+import com.hanghae.hanghaebnb.room.entity.Room;
+import com.hanghae.hanghaebnb.room.repository.RoomRepository;
+import com.hanghae.hanghaebnb.users.entity.Users;
+import com.hanghae.hanghaebnb.users.entity.UsersRoleEnum;
+import com.hanghae.hanghaebnb.users.mapper.UsersMapper;
+import com.hanghae.hanghaebnb.users.repository.UserRepository;
+
+@SpringBootTest
+@DisplayName("Redisson Lock 테스트")
+public class BookServiceTest {
+
+	@Autowired
+	private RoomRepository roomRepository;
+
+	@Autowired
+	private UsersMapper usersMapper;
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Autowired
+	private BookRepository bookRepository;
+
+	@Autowired
+	private LettuceRockBookFacade lettuceRockBookFacade;
+
+	@Test
+	public void bookAtSameTime() throws InterruptedException {
+
+		Users user = usersMapper.toUsers("host@naver.com", "dPwls12!", "철수", UsersRoleEnum.USER);
+		userRepository.save(user);
+
+		Room room = new Room("테스트룸", "테스트테스트", 100000L, 100000L, "test", 0, 0, "이미지", 0, user);
+		roomRepository.save(room);
+
+		RequestBook requestBook = new RequestBook(1L, LocalDate.parse("2023-02-01"), LocalDate.parse("2023-02-03"), 2L, 200000L);
+
+		int threadCount = 100;
+		ExecutorService executorService = Executors.newFixedThreadPool(32);
+
+		CountDownLatch latch = new CountDownLatch(threadCount);
+
+
+		for(int i=0; i<threadCount; i++){
+			int finalI = i;
+			executorService.submit(() -> {
+				try {
+					lettuceRockBookFacade.addBook(1L, requestBook, user);
+				} catch (InterruptedException e) {
+					throw new RuntimeException(e);
+				} finally {
+					latch.countDown();
+				}
+			});
+		}
+		latch.await();
+
+
+		System.out.println(bookRepository.count());
+
+	}
+
+}
